dab6212f4f66ae7450d862502e938e687b5ca364
gumyo <theo.argentais@outlook.fr>
2017-01-14 00:34:11 +0100

    Add files via upload

--- josselin/FonctionTimer.py
+++ josselin/FonctionTimer.py
@@ -2,9 +2,12 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-scoreRelativeTime, phyRelativeTime = 0, 0
+scoreRelativeTime = 0
+phyRelativeTime = 0
 
-def time(commandTimer, scoreRelativeTime, phyRelativeTime):
+def time(commandTimer):
+    global scoreRelativeTime
+    global phyRelativeTime
     if commandTimer == 'pause':
         scoreRelativeTime += pygame.time.get_ticks()
         phyRelativeTime += pygame.time.get_ticks()
@@ -32,19 +35,17 @@ def time(commandTimer, scoreRelativeTime, phyRelativeTime):
         while scoreTime >=1000:
             sec += 1
             scoreTime -= 1000
-        return scoreRelativeTime, phyRelativeTime, dmin, min, dsec, sec
+        return dmin, min, dsec, sec
     elif commandTimer == 'phyTime':
-        return scoreRelativeTime, phyRelativeTime, (pygame.time.get_ticks() + phyRelativeTime)
-    return scoreRelativeTime, phyRelativeTime
+        return (pygame.time.get_ticks() + phyRelativeTime)
 
 while True:
-    a = input('Commande :')
-    b = time(a, scoreRelativeTime, phyRelativeTime)
-    scoreRelativeTime, phyRelativeTime = b[0], b[1]
+    a = input('Commande [pause/play/reset/scoreReset/phyReset/scoreTime/phyTime] :')
+    b = time(a)
     if a == 'phyTime':
-        print(b[2])
+        print(b)
     elif a == 'scoreTime':
-        print(b[2], '[dizaines de minutes]')
-        print(b[3], '[minutes]')
-        print(b[4], '[dizaines de secondes]')
-        print(b[5], '[unités de secondes]')
+        print(b[0], '[dizaines de minutes]')
+        print(b[1], '[minutes]')
+        print(b[2], '[dizaines de secondes]')
+        print(b[3], '[unités de secondes]')
