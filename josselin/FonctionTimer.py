5ec4c0564dad6a1d41dae89b63e363bac7e26868
gumyo <theo.argentais@outlook.fr>
2017-01-17 15:51:38 +0100

    Add files via upload

--- josselin/FonctionTimer.py
+++ josselin/FonctionTimer.py
@@ -22,30 +22,13 @@ def time(commandTimer):
     elif commandTimer == 'phyReset':
         phyRelativeTime = 0 - pygame.time.get_ticks()
     elif commandTimer == 'scoreTime':
-        scoreTime, dmin, min, dsec, sec = (pygame.time.get_ticks() + scoreRelativeTime), 0, 0, 0, 0
-        while scoreTime >= 600000:
-            dmin += 1
-            scoreTime -= 600000
-        while scoreTime >= 60000:
-            min += 1
-            scoreTime -= 60000
-        while scoreTime >= 10000:
-            dsec += 1
-            scoreTime -= 10000
-        while scoreTime >=1000:
-            sec += 1
-            scoreTime -= 1000
-        return dmin, min, dsec, sec
+        return (pygame.time.get_ticks() + scoreRelativeTime)
     elif commandTimer == 'phyTime':
         return (pygame.time.get_ticks() + phyRelativeTime)
 
 while True:
     a = input('Commande [pause/play/reset/scoreReset/phyReset/scoreTime/phyTime] :')
-    b = time(a)
-    if a == 'phyTime':
-        print(b)
-    elif a == 'scoreTime':
-        print(b[0], '[dizaines de minutes]')
-        print(b[1], '[minutes]')
-        print(b[2], '[dizaines de secondes]')
-        print(b[3], '[unités de secondes]')
+    if a == 'phyTime' or a == 'scoreTime':
+        print(time(a))
+    else:
+        time(a)
