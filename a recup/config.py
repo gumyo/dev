8b331b2547cf9be67fc12299f9409802c4ebe45c
gumyo <theo.argentais@outlook.fr>
2017-05-29 09:58:45 +0200

    arecup arch

---
+++ a recup/config.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame import key
+
+def police(memory):
+    memory[0], memory[1], memory[2], memory[3], memory[4] = 0xF0, 0x90, 0x90, 0x90, 0xF0 # O
+    memory[5], memory[6], memory[7], memory[8], memory[9] = 0x20, 0x60, 0x20, 0x20, 0x70 # 1
+    memory[10], memory[11], memory[12], memory[13], memory[14] = 0xF0, 0x10, 0xF0, 0x80, 0xF0 # 2
+    memory[15], memory[16], memory[17], memory[18], memory[19] = 0xF0, 0x10, 0xF0, 0x10, 0xF0 # 3
+    memory[20], memory[21], memory[22], memory[23], memory[24] = 0x90, 0x90, 0xF0, 0x10, 0x10 # 4
+    memory[25], memory[26], memory[27], memory[28], memory[29] = 0xF0, 0x80, 0xF0, 0x10, 0xF0 # 5
+    memory[30], memory[31], memory[32], memory[33], memory[34] = 0xF0, 0x80, 0xF0, 0x90, 0xF0 # 6
+    memory[35], memory[36], memory[37], memory[38], memory[39] = 0xF0, 0x10, 0x20, 0x40, 0x40 # 7
+    memory[40], memory[41], memory[42], memory[43], memory[44] = 0xF0, 0x90, 0xF0, 0x90, 0xF0 # 8
+    memory[45], memory[46], memory[47], memory[48], memory[49] = 0xF0, 0x90, 0xF0, 0x10, 0xF0 # 9
+    memory[50], memory[51], memory[52], memory[53], memory[54] = 0xF0, 0x90, 0xF0, 0x90, 0x90 # A
+    memory[55], memory[56], memory[57], memory[58], memory[59] = 0xE0, 0x90, 0xE0, 0x90, 0xE0 # B
+    memory[60], memory[61], memory[62], memory[63], memory[64] = 0xF0, 0x80, 0x80, 0x80, 0xF0 # C
+    memory[65], memory[66], memory[67], memory[68], memory[69] = 0xE0, 0x90, 0x90, 0x90, 0xE0 # D
+    memory[70], memory[71], memory[72], memory[73], memory[74] = 0xF0, 0x80, 0xF0, 0x80, 0xF0 # E
+    memory[75], memory[76], memory[77], memory[78], memory[79] = 0xF0, 0x80, 0xF0, 0x80, 0x80 # F
+    return memory
+
+def timers(delay, sound):
+    if delay != 0:
+        delay -= 1
+    if sound != 0:
+        sound -= 1
+
+def iniVar():
+    memory = [0] * 4096
+    memory = romOpen(memory)
+    register = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0}
+    keys = [pygame.K_KP0, pygame.K_KP1, pygame.K_KP2, pygame.K_KP3, pygame.K_KP4,pygame.K_KP5, pygame.K_KP6, pygame.K_KP7,
+            pygame.K_KP8, pygame.K_KP9, pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f]
+    stack = [0] * 16
+    display = []
+    for i in range(32): display.append([0] * 64)
+    point_pile = 512
+    point_stack = 0
+    regI = 0
+    delay = 0
+    sound = 0
+    return display, point_stack, point_pile, register, regI, delay, sound, memory, stack
+
+def romOpen(memory):
+    bin = open('PONG', "rb").read()
+    n = 0
+    while n < len(bin):
+        memory[n + 0x200] = bin[n]
+        n = n + 1
+    return memory
---
+++ a recup/cpu.py
@@ -0,0 +1,118 @@
+import instruction as iN
+from config import iniVar, timers
+import pygame
+from random import randint
+from pygame import *
+
+def opCode(point_pile, memory):
+	return (memory[point_pile] << 8) + memory[point_pile + 1]
+
+def id(opcode):
+	mask_block_un = 0xF000
+	mask_block_deux = 0xF00F
+	mask_block_trois = 0xF0FF
+	masks = (mask_block_un, mask_block_deux, mask_block_trois)
+	block_instructions_un = [0x1000, 0x2000, 0x3000, 0x4000, 0x5000, 0x6000, 0x7000, 0x9000, 0xA000, 0xB000, 0xC000, 0xD000]
+	block_instructions_deux = [0x8000, 0x8001, 0x8002, 0x8003, 0x8004, 0x8005, 0x8006, 0x8007, 0x800E]
+	block_instructions_trois = [0x00E0, 0x00EE, 0xE09E, 0xE0A1, 0xF007, 0xF00A, 0xF015, 0xF018, 0xF01E, 0xF029, 0xF033, 0xF055, 0xF065]
+	instructions = (block_instructions_un, block_instructions_deux, block_instructions_trois)
+	for i in range(3):
+		for nombre in range(len(instructions[i])):
+			if opcode & masks[i] == instructions[i][nombre]:
+				return instructions[i][nombre]
+
+
+
+def mainCPU():
+	global display, point_stack, point_pile, register, regI, delay, sound, memory, stack, screen
+	opcode = opCode(point_pile, memory)
+	instruction = id(opcode)
+	if instruction == 0x00E0:
+		display = iN.clear()
+	elif instruction == 0x00EE:
+		point_stack, point_pile, stack = iN.ret(point_stack, point_pile, stack)
+	elif instruction == 0x1000:
+		point_pile = iN.jump(opcode, point_pile)
+	elif instruction == 0x2000:
+		point_pile, point_stack, stack = iN.call(opcode, point_pile, point_stack, stack)
+	elif instruction == 0x3000:
+		point_pile = iN.next(opcode, register, point_pile)
+	elif instruction == 0x4000:
+		point_pile = iN.nextNot(opcode, register, point_pile)
+	elif instruction == 0X5000:
+		point_pile = iN.nextReg(opcode, register, point_pile)
+	elif instruction == 0x6000:
+		register = iN.setNnVx(opcode, register)
+	elif instruction == 0x7000:
+		register = iN.addNnVx(opcode, register)
+	elif instruction == 0x8000:
+		register = iN.setVxVy(opcode, register)
+	elif instruction == 0x8001:
+		register = setVxOrVy(opcode, register)
+	elif instruction == 0x8002:
+		register = iN.setVxAndVy(opcode, register)
+	elif instruction == 0x8003:
+		register = iN.setVxOrVy(opcode, register)
+	elif instruction == 0x8004:
+		register = iN.addVxVy(opcode, register)    
+	elif instruction == 0x8005:
+		register = iN.subVyVx(opcode, register)    
+	elif instruction == 0x8006:
+		register = iN.decVxRight(opcode, register)    
+	elif instruction == 0x8007:
+		register = iN.subVxVy(opcode, register)    
+	elif instruction == 0x800E:
+		register = iN.decVxLeft(opcode, register)    
+	elif instruction == 0x9000:
+		point_pile = iN.jmIfVxNoVy(opcode, register, point_pile)
+	elif instruction == 0xA000:
+		regI = iN.nnnI(opcode, register, regI)
+	elif instruction == 0xB000:
+		point_pile = iN.nnnVo
+	elif instruction == 0xC000:
+		register = iN.rndVx(opcode, register)
+	elif instruction == 0xD000:
+		display = iN.drawDisplay(opcode, register, screen, display, memory, regI)
+	elif instruction == 0xE09E:
+		point_pile = iN.keyVxYes(opcode, register, keys)
+	elif instruction == 0xE0A1:
+		point_pile = iN.keyVxNo(opcode, register, keys)
+	elif instruction == 0xF007:
+		register = iN.tmpVx(opcode ,register, delay)
+	elif instruction == 0xF00A:
+		register = iN.key(opcode, register, keys)
+	elif instruction == 0xF015:
+		delay = iN.vxTmp(opcode, register, delay)
+	elif instruction == 0xF018:
+		sound = iN.soundVx(opcode, register, sound)
+	elif instruction == 0xF01E:
+		regI, register = iN.addIVx(opcode, register, regI)
+	elif instruction == 0xF029:
+		regI = iN.IdgtVx(opcode, register, regI)
+	elif instruction == 0xF033:
+		memory = iN.stkVxI(opcode, register, regI, memory)
+	elif instruction == 0xF055:
+		memory = iN.stkVoVx(opcode, register, regI, memory)
+	elif instruction == 0xF065:
+		memory = iN.repVoVx(opcode, register, regI, memory)
+	point_pile += 2
+
+
+def main():
+	global display, point_stack, point_pile, register, regI, delay, sound, memory, stack, screen
+	pygame.init()
+	screen = pygame.display.set_mode((640,320))
+	display, point_stack, point_pile, register, regI, delay, sound, memory, stack = iniVar()
+	timer = pygame.USEREVENT + 1
+	pygame.time.set_timer(timer, 17)
+	while True:
+		mainCPU()
+		for event in pygame.event.get():
+			if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+				pygame.quit()
+				sys.exit()
+			if event.type == timer:
+				timers(delay, sound)
+	pygame.quit()
+
+main()
